17b6bedfda6c28346d3301a7b3f32e33dfade2c9
Elena Bakherkina <lll.00088@mail.ru>
2025-07-17 21:54:18 +0300

    KS-2-1 refactor

--- src/main/kotlin/lesson2/lesson2_1.kt
+++ src/main/kotlin/lesson2/lesson2_1.kt
@@ -1,7 +1,20 @@
 package org.example.lesson2
 
 fun main() {
-    val averageScore: Double = (3.0 + 4.0 + 3.0 + 5.0) / 4.0
+
+    val numberOfNumbers = 4.0
+    val studentAssessment1 = 3.0
+    val studentAssessment2 = 4.0
+    val studentAssessment3 = 3.0
+    val studentAssessment4 = 5.0
+
+    val averageScore: Double = (
+            studentAssessment1 + 
+            studentAssessment2 + 
+            studentAssessment3 +
+            studentAssessment4
+            ) / numberOfNumbers
+
     val FormatAverageScore = String.format("%.2f", averageScore)
     println(FormatAverageScore)
 }
