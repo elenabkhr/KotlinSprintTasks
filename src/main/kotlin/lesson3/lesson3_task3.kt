2873d54d886e7e415e9dc0fc76928d75266d68c5
Elena Bakherkina <lll.00088@mail.ru>
2025-07-18 21:05:39 +0300

    KS-3-3

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson3
+
+fun main() {
+    var number = 5
+
+    var multiplicationTable: String = """
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+        $number * 10 = ${number * 10}
+    """.trimIndent()
+
+    println(multiplicationTable)
+}
