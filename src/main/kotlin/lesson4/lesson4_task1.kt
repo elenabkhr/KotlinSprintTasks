71e979867fe09af77bac78695820712617a61230
Elena Bakherkina <lll.00088@mail.ru>
2025-07-19 23:07:38 +0300

    KS-4-1 refactor

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,12 +1,13 @@
 package org.example.lesson4
+
 const val NUMBER_OF_TABLES = 13
 
 fun main() {
-    var reservedTablesForToday = 13
-    var optionToReservedToday = reservedTablesForToday != NUMBER_OF_TABLES
+    val reservedTablesForToday = 13
+    val optionToReservedToday = reservedTablesForToday < NUMBER_OF_TABLES
 
-    var reservedTablesForTomorrow = 9
-    var optionToReserved = reservedTablesForTomorrow != NUMBER_OF_TABLES
+    val reservedTablesForTomorrow = 9
+    val optionToReserved = reservedTablesForTomorrow < NUMBER_OF_TABLES
 
     println("Доступность столиков на сегодня: $optionToReservedToday\n" +
             "Доступность столиков на завтра: $optionToReserved")
