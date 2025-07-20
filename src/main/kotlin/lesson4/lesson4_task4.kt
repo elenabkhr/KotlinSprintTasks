22a6ced6312e6c8b8c66d6d3757da06deb3b920b
Elena Bakherkina <lll.00088@mail.ru>
2025-07-20 23:35:05 +0300

    KS-4-4 refactor

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,10 +2,12 @@ package org.example.lesson4
 
 fun main() {
     val trainingDay = 5
-    val armExercises = trainingDay % 2 != 0
-    val absExercises = trainingDay % 2 != 0
-    val backExercises = trainingDay % 2 == 0
-    val legExercises = trainingDay % 2 == 0
+    val isEven: Boolean = (trainingDay % 2 == 0)
+
+    val armExercises = !isEven
+    val absExercises = !isEven
+    val backExercises = isEven
+    val legExercises = isEven
 
     println(
         "Упражнения для рук: $armExercises\nУпражнения для ног: $absExercises\nУпражнения для спины: $backExercises" +
