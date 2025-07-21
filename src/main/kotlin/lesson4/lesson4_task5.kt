11f8dd13a5902740003ae01d3fb7199c89c7d9c5
Elena Bakherkina <lll.00088@mail.ru>
2025-07-21 20:48:21 +0300

    KS-4-5 refactor

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,22 +1,34 @@
 package org.example.lesson4
 
+const val MAX_NUMBER_OF_PERSON = 70
+const val MIN_NUMBER_OF_PERSON = 55
+const val RECOMMEND_BOXES_OF_PROVISIONS = 50
+
 fun main() {
+    println("Имеет ли корабль повреждения?")
     val hasDamage = readln().toBoolean()
+
+    println("Какое число экипажа корабля?")
     val numberOfPerson = readln().toInt()
+
+    println("Какое число ящиков провизии?")
     val boxesOfProvisions = readln().toInt()
+
+    println("Благоприятная ли погода?")
     val hasFavorableWeather = readln().toBoolean()
 
-    if (!hasDamage) {
-        if ((numberOfPerson in 55..71) && (boxesOfProvisions > 50)) {
-            println("Корабль может отправиться в плавание.")
-        } else {
-            println("Корабль не может отправиться в плавание.")
-        }
+    if ((!hasDamage &&
+                numberOfPerson in MIN_NUMBER_OF_PERSON..MAX_NUMBER_OF_PERSON &&
+                boxesOfProvisions > RECOMMEND_BOXES_OF_PROVISIONS
+                ) ||
+        (hasDamage &&
+                numberOfPerson == MAX_NUMBER_OF_PERSON &&
+                hasFavorableWeather &&
+                boxesOfProvisions >= RECOMMEND_BOXES_OF_PROVISIONS
+                )
+    ) {
+        println("Корабль может отправиться в плавание.")
     } else {
-        if ((numberOfPerson == 70) && (boxesOfProvisions >= 50) && (hasFavorableWeather)) {
-            println("Корабль может отправиться в плавание.")
-        } else {
-            println("Корабль не может отправиться в плавание.")
-        }
+        println("Корабль не может отправиться в плавание.")
     }
 }
