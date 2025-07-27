d03881335d801f734c5338bdddaceba8488fd704
Elena Bakherkina <lll.00088@mail.ru>
2025-07-28 00:15:39 +0300

    KS-8-3

---
+++ src/main/kotlin/lesson8/lesson8_task3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson8
+
+fun main() {
+    val pizzaIngredients = arrayOf("Основа из теста", "Бекон", "Сыр", "Томат", "Зелель", "Соус")
+
+    println("Введите ингредиент для поиска: ")
+    val ingredientSearch = readln()
+
+    val resultSearch = pizzaIngredients.indexOf(ingredientSearch)
+
+    if (resultSearch >= 0) println("Ингредиент \"$ingredientSearch\" в рецепте есть.")
+    else println("Такого ингредиента в рецепте нет.")
+}
