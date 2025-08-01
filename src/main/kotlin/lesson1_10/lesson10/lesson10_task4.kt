ffc9ae0e66a6170f865321c73f9b8ac910a83c8c
Elena Bakherkina <lll.00088@mail.ru>
2025-08-01 14:50:34 +0300

    KS-10-4 refactor-2

---
+++ src/main/kotlin/lesson1_10/lesson10/lesson10_task4.kt
@@ -0,0 +1,39 @@
+package org.example.lesson1_10.lesson10
+
+fun main() {
+    var winCount = 0
+
+    while (true) {
+        winCount += playRound(userRoll = rollDice2("человек"), computerRoll = rollDice2("машина"))
+        println("Хотите бросить кости еще раз? Введите Да или Нет: ")
+        val playAgain = readln()
+
+        if (playAgain.equals("нет", ignoreCase = true)) {
+            println("Игра завершена")
+            break
+        } else {
+            println("Игра продолжается")
+        }
+    }
+    println("Выиграно партий: $winCount")
+}
+
+fun playRound(userRoll: Int, computerRoll: Int): Int {
+    var userVictory = 0
+
+    when {
+        userRoll > computerRoll -> {
+            println("Победило человечество")
+            userVictory++
+        }
+        userRoll == computerRoll -> println("Победила дружба")
+        else -> println("Победила машина")
+    }
+    return userVictory
+}
+
+fun rollDice2(playerName: String): Int {
+    val userAction = (1..6).random()
+    println("Бросил(а) $playerName, значение на кубиках: $userAction")
+    return userAction
+}
