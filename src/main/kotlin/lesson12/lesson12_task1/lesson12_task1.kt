b12933c6909209f3eff937258cf43347be2e692d
Elena Bakherkina <lll.00088@mail.ru>
2025-08-03 15:37:46 +0300

    KS-12-1 refactor

--- src/main/kotlin/lesson12/lesson12_task1/lesson12_task1.kt
+++ src/main/kotlin/lesson12/lesson12_task1/lesson12_task1.kt
@@ -2,21 +2,21 @@ package org.example.lesson12.lesson12_task1
 
 fun main() {
     val weather1 = Weather()
-    weather1.daytimeTemperature = "1 день"
-    weather1.nightTemperature = "1 ночь"
+    weather1.daytimeTemperature = 15
+    weather1.nightTemperature = 10
     weather1.precipitation = true
     weather1.printAll()
 
     val weather2 = Weather()
-    weather2.daytimeTemperature = "2 день"
-    weather2.nightTemperature = "2 ночь"
+    weather2.daytimeTemperature = 10
+    weather2.nightTemperature = 5
     weather2.precipitation = true
     weather2.printAll()
 }
 
 class Weather() {
-    var daytimeTemperature = "дневная температура"
-    var nightTemperature = "ночная температура"
+    var daytimeTemperature = 0
+    var nightTemperature = 0
     var precipitation = false
 
     fun printAll() {
