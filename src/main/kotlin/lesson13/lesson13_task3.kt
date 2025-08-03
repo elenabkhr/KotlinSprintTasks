184126d7263eb44aa7f86ebd48f884ad4cf8734f
Elena Bakherkina <lll.00088@mail.ru>
2025-08-03 19:07:31 +0300

    KS-13-3

---
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson13
+
+fun main() {
+    val listContact = listOf(
+        PhoneDirectory3("Name1", 89123456111),
+        PhoneDirectory3("Name2", 89123456222),
+        PhoneDirectory3("Name3", 89123456333, "null"),
+        PhoneDirectory3("Name4", 89123456444, "company4"),
+        PhoneDirectory3("Name5", 89123456555, "company5")
+    )
+
+    for (i in listContact) {
+        println("Компания: ${i.company}")
+    }
+}
+
+class PhoneDirectory3(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null ?: "<не указано>"
+)
