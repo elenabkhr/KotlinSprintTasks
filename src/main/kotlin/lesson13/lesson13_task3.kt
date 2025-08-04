1586ffbf468ae2096b1813263b31118498e0524c
Elena Bakherkina <lll.00088@mail.ru>
2025-08-04 14:39:34 +0300

    KS-13-3 refactor

--- src/main/kotlin/lesson13/lesson13_task3.kt
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -4,13 +4,17 @@ fun main() {
     val listContact = listOf(
         PhoneDirectory3("Name1", 89123456111),
         PhoneDirectory3("Name2", 89123456222),
-        PhoneDirectory3("Name3", 89123456333, "null"),
+        PhoneDirectory3("Name3", 89123456333, null),
         PhoneDirectory3("Name4", 89123456444, "company4"),
         PhoneDirectory3("Name5", 89123456555, "company5")
     )
+    val listContactNotNull = listContact.mapNotNull {
+        if (it.company != null && it.company != "<не указано>")
+            it else null
+    }
 
-    for (i in listContact) {
-        println("Компания: ${i.company}")
+    for (i in listContactNotNull) {
+        println(i.company)
     }
 }
 
