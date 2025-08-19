919b10fd6e5a8e56d99055e18972cb36934b747f
Elena Bakherkina <lll.00088@mail.ru>
2025-08-19 21:38:54 +0300

    KS-15-2 refactor

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -1,31 +1,32 @@
 package org.example.lesson15
 
 fun main() {
-    val weatherServer1 = WeatherServer()
-    weatherServer1.sendMessage(Temperature)
-    weatherServer1.sendMessage(PrecipitationAmount)
+    val temperature = Temperature(10)
+    val precipitationAmount = PrecipitationAmount(20)
+
+    val weatherServer = WeatherServer()
+    weatherServer.sendMessage(temperature)
+    weatherServer.sendMessage(precipitationAmount)
 }
 
-class WeatherServer {
+class WeatherServer() {
     fun sendMessage(obj: WeatherStationStats) {
-            (obj.textMessage())
+        (obj.textMessage())
     }
 }
 
 abstract class WeatherStationStats() {
-    abstract fun textMessage ()
+    abstract fun textMessage()
 }
 
-object Temperature : WeatherStationStats() {
-    val temperature = 10
+class Temperature(val temperature: Int) : WeatherStationStats() {
 
     override fun textMessage() {
         println("Температура: $temperature")
     }
 }
 
-object PrecipitationAmount : WeatherStationStats() {
-    val precipitationAmount = 20
+class PrecipitationAmount(val precipitationAmount: Int) : WeatherStationStats() {
 
     override fun textMessage() {
         println("Количество осадков: $precipitationAmount")
