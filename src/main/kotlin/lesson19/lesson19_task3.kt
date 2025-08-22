947ef1a53d82daa3b25912fd51c429fbc174e0e4
Elena Bakherkina <lll.00088@mail.ru>
2025-08-23 00:23:14 +0300

    KS-19-3

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson19
+
+class SpaceShip() {
+    fun takeOff() {
+        TODO("Добавить логику")
+    }
+
+    fun land() {
+        throw NotImplementedError()
+    }
+
+    fun shootAsteroid() {}
+}
