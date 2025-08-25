c0f91499f83344917ae6c0259e03b803efbe90b0
Elena Bakherkina <lll.00088@mail.ru>
2025-08-25 19:34:00 +0300

    KS-19-2 refactor

--- src/main/kotlin/lesson19/lesson19_task2.kt
+++ src/main/kotlin/lesson19/lesson19_task2.kt
@@ -1,27 +1,30 @@
 package org.example.lesson19
 
-enum class Product {
-    CLOTHES {
-        override fun getCategory(): String {
-            return ("Одежда")
-        }
-    },
-    OFFICE_SUPPLIES {
-        override fun getCategory(): String {
-            return ("Канцелярские товары")
-        }
-    },
-    DIFFERENT {
-        override fun getCategory(): String {
-            return ("Разное")
-        }
-    };
+fun main() {
+    val product1 = ProductDescription("name1", 1, Product.CLOTHES)
+    product1.getProductInfo()
 
-    abstract fun getCategory(): String
+    val product2 = ProductDescription("name2", 2, Product.OFFICE_SUPPLIES)
+    product2.getProductInfo()
+
+    val product3 = ProductDescription("name2", 3, Product.DIFFERENT)
+    product3.getProductInfo()
+}
+
+enum class Product {
+    CLOTHES,
+    OFFICE_SUPPLIES,
+    DIFFERENT;
+
+    fun getCategoryName(): String = when (this) {
+        CLOTHES -> "одежда"
+        OFFICE_SUPPLIES -> "канцелярские товары"
+        DIFFERENT -> "разное"
+    }
 }
 
 class ProductDescription(val name: String, val id: Int, val category: Product) {
     fun getProductInfo() {
-        println("Название: $name\nid: $id\nКатегория: ${category.getCategory()}")
+        println("Название: $name, id: $id, категория: ${category.getCategoryName()}")
     }
 }
