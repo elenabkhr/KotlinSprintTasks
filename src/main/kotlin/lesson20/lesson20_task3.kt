525370a25f3b6ea37b9edf85754753711197d698
Elena Bakherkina <lll.00088@mail.ru>
2025-08-26 18:43:28 +0300

    KS-20-3

--- src/main/kotlin/lesson20/lesson20_task3.kt
+++ src/main/kotlin/lesson20/lesson20_task3.kt
@@ -2,7 +2,7 @@ package org.example.lesson20
 
 class Player3(val hasKey: Boolean) {
     val checkKey: (Player3) -> String = { player ->
-        if (hasKey) "Игрок открыл дверь"
+        if (player.hasKey) "Игрок открыл дверь"
         else "Дверь заперта"
     }
 }
