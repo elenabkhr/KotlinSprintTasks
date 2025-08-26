e0aa73d96c50844253f9e64588c8ce6550142579
Elena Bakherkina <lll.00088@mail.ru>
2025-08-26 17:23:48 +0300

    KS-20-2 refactor 2

--- src/main/kotlin/lesson20/lesson20_task2.kt
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -2,12 +2,12 @@ package org.example.lesson20
 
 fun main() {
     val player = Player("player", 5, 10)
-    player.reshoreHealth.invoke(player)
+    player.reshoreHealth(player)
 }
 
 class Player(val name: String, var currentHealth: Int, val maxHealth: Int) {
     val reshoreHealth: (Player) -> Unit = { it ->
-        currentHealth = maxHealth
+        it.currentHealth = it.maxHealth
         println("Здоровье игрока $name восстановлено. Здоровье сейчас: $currentHealth")
     }
 }
