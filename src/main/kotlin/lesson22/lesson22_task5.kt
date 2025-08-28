6ace550b1bb7efd13d4a24a5e73a985e7ee1d4dc
Elena Bakherkina <lll.00088@mail.ru>
2025-08-28 21:27:15 +0300

    KS-22-5 refactor

--- src/main/kotlin/lesson22/lesson22_task5.kt
+++ src/main/kotlin/lesson22/lesson22_task5.kt
@@ -9,7 +9,10 @@ fun main() {
         LocalDateTime.of(2025, 8, 28, 1, 20, 0),
         4.22,
     )
-    println(alphaCentauri)
+    println("Название места или события: ${alphaCentauri.component1()}")
+    println("Описание места или события: ${alphaCentauri.component2()}")
+    println("Дата и время события: ${alphaCentauri.component3()}")
+    println("Расстояние места или события от Земли в световых годах: ${alphaCentauri.component4()}")
 }
 
 data class GalacticGuide(
