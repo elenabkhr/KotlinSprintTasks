3c4de0e3914ca8da8cd2bf13bfddbad7de95edaf
Elena Bakherkina <lll.00088@mail.ru>
2025-09-03 18:57:11 +0300

    KS-10-5 refactor

--- src/main/kotlin/lesson1_10/lesson10/lesson10_task5.kt
+++ src/main/kotlin/lesson1_10/lesson10/lesson10_task5.kt
@@ -1,27 +1,22 @@
 package org.example.lesson1_10.lesson10
 
-import kotlin.random.Random
-
 const val USER_LOGIN = "login"
 const val USER_PASSWORD = "password"
 
 fun main() {
-    println(getCart(authorizeUser("login", "password")))
-    println(getCart(authorizeUser("log", "pas")))
+    val token = authorizeUser("login", "password")
+    if (token != null) {
+        getCart(token)
+    } else {
+        println("Неудачная авторизация")
+    }
 }
 
 fun authorizeUser(login: String, password: String): String? {
-    var accessToken = ""
 
     if (login == USER_LOGIN && password == USER_PASSWORD) {
-        val rangeLowercaseLetter = 'a'..'z'
-        val rangeUppercaseLetter = 'A'..'Z'
-
-        for (i in 1..32) {
-            accessToken += (rangeUppercaseLetter + rangeLowercaseLetter +
-                    Random.nextInt(0, 9).toString()).random()
-        }
-        println(accessToken)
+        val allChars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val accessToken = List(32) { allChars.random() }.joinToString("")
         return accessToken
 
     } else {
@@ -29,9 +24,7 @@ fun authorizeUser(login: String, password: String): String? {
     }
 }
 
-fun getCart(accessToken: String?): Any {
-    val cartContents = mutableListOf("mirror", "toy", "vase")
-
-    if (accessToken != null) return cartContents
-    else return "Неудачная авторизация, данные введены неверно"
+fun getCart(accessToken: String): String {
+    val cartContents = listOf("mirror", "toy", "vase").toString()
+    return cartContents
 }
